3e803fa4b13cd4d6110dacd91a9720e5967b94d0
Matthew Pantel <79867801+TypeErrorDev@users.noreply.github.com>
2024-11-14 14:57:33 -0800

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Home from "./Components/Home";
 
 function App() {
   return (
-    <div className="flex justify-center items-center bg-[#5f411f] text-white">
+    <div className="flex justify-center items-center h-screen bg-[#362f26] text-white">
       <Home />
     </div>
   );
