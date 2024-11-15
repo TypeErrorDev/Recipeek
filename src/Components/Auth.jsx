f4f1a21c3c4c55a24c888d3f59eba8eed7cb8e87
Matthew Pantel <79867801+TypeErrorDev@users.noreply.github.com>
2024-11-14 20:16:09 -0800

    just starting the styling

--- src/Components/Auth.jsx
+++ src/Components/Auth.jsx
@@ -1,6 +1,6 @@
 const Auth = () => {
   return (
-    <div className="flex justify-center items-center mt-5 bg-foreground-light h-[500px] w-[300px] rounded-lg border shadow-main_text-dark shadow-xl">
+    <div className="flex justify-center items-center mt-5 bg-foreground-light h-[300px] w-[300px] rounded-lg border-2 shadow-main_text-dark shadow-xl">
       Testings
     </div>
   );
